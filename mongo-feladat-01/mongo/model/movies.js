c4b8bc07250810487c1af078b31ab24fc981d260
Hajni83 <maro.hajnalka@gmail.com>
2022-05-07 07:08:09 +0200

    rating and releaseYear

--- mongo-feladat-01/mongo/model/movies.js
+++ mongo-feladat-01/mongo/model/movies.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 var moviesSchema = new mongoose.Schema({
     title:String,
     category:String,
-    director:String
+    director:String,
+    rating:Array,
+    releaseYear:Number
 });
 
 module.exports= mongoose.model('movies', moviesSchema);
